e71260aac0680b3776c36fe807a4a7e62ef03492
lihanyi <lihanyi@narzh.com>
2016-05-25 18:56:09 +0800

    mod

--- account/models.py
+++ account/models.py
@@ -33,7 +33,7 @@ ROLE2NAME = dict(ROLES)
 class UserProfile(models.Model):
 	user = models.ForeignKey(User, unique=True)
 	manager_id = models.IntegerField(default=0) #创建该用户的系统用户的id
-	role = models.IntegerField(choices=ROLES) #角色
+	role = models.IntegerField(default=CUSTOMER,choices=ROLES) #角色
 	name = models.CharField(max_length=32) #账号名称
 	is_active = models.BooleanField(default=True, verbose_name='用户是否有效')
 	note = models.CharField(max_length=1024, default='') #备注
