c4a6c975ee6fd7a1450f5c00520f678b8987787c
lihanyi <lihanyi@narzh.com>
2016-05-26 17:42:22 +0800

    fix

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,45 +6,73 @@ from account.models import *
 # top_navs : 获得top nav集合
 #===============================================================================
 def top_navs(request):
-	role = UserProfile.objects.get(user_id=request.user.id).role
-	print(role)
-	if role == CUSTOMER:
-		top_navs = [{
-			'name': 'order',
-			'displayName': '订单',
-			'icon': 'list-alt',
-			'href': '/order/datas/'
-		}, {
-			'name': 'product',
-			'displayName': '商品',
-			'icon': 'credit-card',
-			'href': '/product/product_list/'
-		}, {
-			'name': 'fans',
-			'displayName': '粉丝投放',
-			'icon': 'cog',
-			'href': '#'
-		}, {
-			'name': 'reconcile',
-			'displayName': '对账',
-			'icon': 'cog',
-			'href': '#'
-		}]
-	elif role == AGENCY:
-		top_navs = []
-	elif role == YUN_YING:
-		top_navs = [{
-			'name': 'order',
-			'displayName': '订单',
-			'icon': 'list-alt',
-			'href': '/order/datas/'
-		}, {
-			'name': 'product',
-			'displayName': '商品',
-			'icon': 'credit-card',
-			'href': '/product/product_list/'
-		}]
-	else:
+	try:
+		role = UserProfile.objects.get(user_id=request.user.id).role
+		if role == CUSTOMER:
+			top_navs = [{
+				'name': 'order',
+				'displayName': '订单',
+				'icon': 'list-alt',
+				'href': '/order/datas/'
+			}, {
+				'name': 'product',
+				'displayName': '商品',
+				'icon': 'credit-card',
+				'href': '/product/product_list/'
+			}, {
+				'name': 'fans',
+				'displayName': '粉丝投放',
+				'icon': 'cog',
+				'href': '#'
+			}, {
+				'name': 'reconcile',
+				'displayName': '对账',
+				'icon': 'cog',
+				'href': '#'
+			}]
+		elif role == AGENCY:
+			top_navs = []
+		elif role == YUN_YING:
+			top_navs = [{
+				'name': 'order',
+				'displayName': '订单',
+				'icon': 'list-alt',
+				'href': '/order/datas/'
+			}, {
+				'name': 'product',
+				'displayName': '商品',
+				'icon': 'credit-card',
+				'href': '/product/product_list/'
+			}]
+		else:
+			top_navs = [{
+				'name': 'order',
+				'displayName': '订单',
+				'icon': 'list-alt',
+				'href': '/order/datas/'
+			}, {
+				'name': 'product',
+				'displayName': '商品',
+				'icon': 'credit-card',
+				'href': '/product/product_list/'
+			}, {
+				'name': 'fans',
+				'displayName': '粉丝投放',
+				'icon': 'cog',
+				'href': '#'
+			}, {
+				'name': 'reconcile',
+				'displayName': '对账',
+				'icon': 'cog',
+				'href': '#'
+			}, {
+				'name': 'manager',
+				'displayName': '账号管理',
+				'icon': 'cog',
+				'href': '/manager/account/'
+			}]
+		return {'top_navs': top_navs}
+	except:
 		top_navs = [{
 			'name': 'order',
 			'displayName': '订单',
