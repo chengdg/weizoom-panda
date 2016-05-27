640693a7874403832b08480387561ed9b42d1645
huangjian <huangjian@narzh.com>
2016-05-27 10:58:14 +0800

    bundle url

--- panda/settings.py
+++ panda/settings.py
@@ -220,7 +220,7 @@ LOGIN_URL = '/account/login/'
 if 'develop' == MODE:
     import logging
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
-    WEBPACK_BUNDLE_JS = 'http://127.0.0.1:4188/static/build/bundle.js'
+    WEBPACK_BUNDLE_JS = 'http://127.0.0.1:4180/static/build/bundle.js'
     DEBUG = True
 elif 'test' == MODE:
     WEBPACK_BUNDLE_JS = '/static/build/bundle.js'
