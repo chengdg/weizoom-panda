ef4fd178ea9b34792caa1daaf2eeea1dfc0f11f1
huangjian <huangjian@narzh.com>
2016-06-16 18:47:38 +0800

    客户概况销量

--- customer/customer.py
+++ customer/customer.py
@@ -17,7 +17,10 @@ from resource import models as resource_models
 from product import models as product_models
 from account.models import *
 from util import string_util
+from panda.settings import ZEUS_HOST
 import nav
+import urllib2
+import urllib
 
 FIRST_NAV = 'customer'
 SECOND_NAV = 'customer'
@@ -41,25 +44,54 @@ class Customer(resource.Resource):
 
 	def api_get(request):
 		cur_page = request.GET.get('page', 1)
-		rows = []
 		user_profiles = UserProfile.objects.filter(role=1) #role{1:客户}
 		products = product_models.Product.objects.all()
-		user_id2product_name = {}
+		# user_id2product_name = {}
+		# for product in products:
+		# 	if product.owner_id not in user_id2product_name:
+		# 		user_id2product_name[product.owner_id] = [product.product_name]
+		# 	else:
+		# 		user_id2product_name[product.owner_id].append(product.product_name)
+
+		user_id2product_id = {}
 		for product in products:
-			if product.owner_id not in user_id2product_name:
-				user_id2product_name[product.owner_id] = [product.product_name]
+			if product.owner_id not in user_id2product_id:
+				user_id2product_id[product.owner_id] = [product.id]
 			else:
-				user_id2product_name[product.owner_id].append(product.product_name)
+				user_id2product_id[product.owner_id].append(product.id)
+
+		product_id2name = {product.id:product.product_name for product in products}
 		pageinfo, user_profiles = paginator.paginate(user_profiles, cur_page, 10, query_string=request.META['QUERY_STRING'])
 
+		products = product_models.Product.objects.all().order_by('-id')
+		product_ids = ['%s'%product.id for product in products]
+		if len(product_ids)>1:
+			product_ids = '_'.join(product_ids)
+		# 请求接口获得数据
+		url = ZEUS_HOST+'/mall/product_sales/?product_ids='+product_ids
+		url_request = urllib2.Request(url)
+		opener = urllib2.urlopen(url_request)
+		sales = []
+		res = opener.read()
+		if json.loads(res)['code'] == 200:
+			sales = json.loads(res)['data']
+		else:
+			response = create_response(500)
+			return response.get_response()
+
+		rows = []
 		for user in user_profiles:
-			product_name = [] if user.user_id not in user_id2product_name else user_id2product_name[user.user_id]
+			product_ids = [] if user.user_id not in user_id2product_id else user_id2product_id[user.user_id]
 			product_infos = []
-			if product_name:
-				for name in product_name:
+			total_sales = 0
+			if product_ids:
+				for product_id in product_ids:
+					name = '' if product_id not in product_id2name else product_id2name[product_id]
+					sale = 0 if product.id not in sales else sales[product_id]
+					total_sales += sale
 					product_infos.append({
 						'name': name,
-						'sales': 500,
+						'sales': '%s' %sale,
 						'time': '2016-06-01'
 					})
 
@@ -67,7 +99,7 @@ class Customer(resource.Resource):
 				'user_id': user.user_id,
 				'customer_name': user.name,
 				'total_orders': 500,
-				'total_sales': 1500,
+				'total_sales': '%s' %total_sales,
 				'total_fans': 808,
 				'product_infos': json.dumps(product_infos)
 			})
