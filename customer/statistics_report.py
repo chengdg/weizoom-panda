140f93f77b0bb754d5781b48ca314241db1b6218
lihanyi <lihanyi@narzh.com>
2016-06-28 10:26:32 +0800

    入口

--- customer/statistics_report.py
+++ customer/statistics_report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import time
-
+import datetime
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -93,6 +93,7 @@ class StatisticsReport(resource.Resource):
 						webapp_id2store_username[account_zypt_info['webapp_id']] = 'weizoom_xuesheng'
 					else:
 						# webapp_id2store_username[account_zypt_info['webapp_id']] = [account_zypt_info['store_name']]
+						#TODO 为方便测试，先将不算自营平台的算为微众白富美的订单
 						webapp_id2store_username[account_zypt_info['webapp_id']] = 'weizoom_baifumei'
 			else:
 				print(res)
@@ -207,13 +208,6 @@ class StatisticsReportData(resource.Resource):
 				#获得所有绑定过云商通的云商通商品id
 				product_weapp_id2product_id[weapp_product_id] = product_has_relation.product_id
 		
-		product_id2time = {}
-		for product_has_relation in product_has_relations:
-			product_id = product_has_relation.product_id
-			if product_id not in product_id2time:
-				product_id2time[product_id] = [product_has_relation.created_at]
-			else:
-				product_id2time[product_id].append(product_has_relation.created_at)
 		api_pids = '_'.join(api_pids)
 		print('api_pids:')
 		print(api_pids)
@@ -243,6 +237,7 @@ class StatisticsReportData(resource.Resource):
 						webapp_id2store_username[account_zypt_info['webapp_id']] = 'weizoom_xuesheng'
 					else:
 						# webapp_id2store_username[account_zypt_info['webapp_id']] = [account_zypt_info['store_name']]
+						#TODO 为方便测试，先将不算自营平台的算为微众白富美的订单
 						webapp_id2store_username[account_zypt_info['webapp_id']] = 'weizoom_baifumei'
 			else:
 				print(res)
@@ -259,7 +254,13 @@ class StatisticsReportData(resource.Resource):
 		pid2weizoom_shop_sales = {}
 		pid2weizoom_xuesheng_sales = {}
 
+		first_week = 0
+		second_week = 0
+		third_week = 0
+		fourth_week = 0
+
 		if api_pids != '':
+			first_bind_relation_time = product_has_relations.first().created_at
 			params = {'product_ids': api_pids}
 			r = requests.get(ZEUS_HOST+'/panda/order_export/',params=params)
 			res = json.loads(r.text)
@@ -270,8 +271,8 @@ class StatisticsReportData(resource.Resource):
 				response = create_response(500)
 				return response.get_response()
 
-			#获得云商通商品id与对应的自营平台的总销量之间的关系
 			for order in orders:
+				#获得云商通商品id与对应的自营平台的总销量之间的关系
 				store_username = webapp_id2store_username[order['webapp_id']]
 				return_product_infos = order['products'] #返回的订单数据
 				for return_product_info in return_product_infos:
@@ -308,6 +309,16 @@ class StatisticsReportData(resource.Resource):
 					else:
 						pid2weizoom_xuesheng_sales[product_id].append(order['id'])
 
+				#计算订单周销售趋势
+				order_time = datetime.datetime.strptime(order['created_at'],"%Y-%m-%d %H:%M:%S")
+				if first_bind_relation_time< order_time < first_bind_relation_time+datetime.timedelta(weeks=1):
+					first_week += 1
+				elif first_bind_relation_time+datetime.timedelta(weeks=1) < order_time < first_bind_relation_time+datetime.timedelta(weeks=2):
+					second_week += 1
+				elif first_bind_relation_time+datetime.timedelta(weeks=2) < order_time < first_bind_relation_time+datetime.timedelta(weeks=3):
+					third_week += 1
+				elif first_bind_relation_time+datetime.timedelta(weeks=3) < order_time < first_bind_relation_time+datetime.timedelta(weeks=4):
+					fourth_week += 1
 			weizoom_baifumei_orders_number = 0
 			weizoom_club_orders_number = 0
 			weizoom_jia_orders_number = 0
@@ -323,10 +334,10 @@ class StatisticsReportData(resource.Resource):
 				weizoom_xuesheng_orders_number += len(pid2weizoom_xuesheng_sales[product.id]) if pid2weizoom_xuesheng_sales.has_key(product.id) else 0
 
 			rows.append({
-				'first_week': '',
-				'second_week': '',
-				'third_week': '',
-				'fourth_week': '',
+				'first_week': first_week,
+				'second_week': second_week,
+				'third_week': third_week,
+				'fourth_week': fourth_week,
 				'all_purchase_number': '',
 				'one_time_purchase': '',
 				're_purchase':'',
