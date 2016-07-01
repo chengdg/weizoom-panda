4e0b635f08e0342dbd1917e80d8f140fa6fa3103
lihanyi <lihanyi@weizoom.com>
2016-07-01 09:33:25 +0800

    models修改

--- fans/models.py
+++ fans/models.py
@@ -4,16 +4,23 @@ from django.db import models
 # Create your models here.
 
 class Fans(models.Model):
-	fans_id = models.IntegerField(default=0) #粉丝id
+	name = models.CharField(max_length=2048, default='') #粉丝名称
 	fans_url = models.CharField(max_length=2048, default='') #粉丝头像
 	male = models.BooleanField(default=True) #性别,1-男:0-女
 	location = models.CharField(max_length=1024, default='') #所在地
 	purchasing_index = models.FloatField(default=0) #购买指数
 	spread_index = models.FloatField(default=0) #传播指数
-	status = models.IntegerField(default=0) #投放状态
-	related_order_id = models.CharField(max_length=1024, default='') #关联订单号
-	pushed_date = models.DateTimeField() #投放日期
 	created_at = models.DateTimeField(auto_now_add=True) #创建日期
 
 	class Meta(object):
 		db_table = 'fans_fans'
+
+class UserHasFans(models.Model):
+	user_id = models.IntegerField(default=0) #用户id
+	fans_id = models.IntegerField(default=0) #粉丝id
+	status = models.IntegerField(default=0) #投放状态
+	related_order_id = models.CharField(max_length=1024, default='') #关联订单号
+	pushed_date = models.DateTimeField() #投放日期
+
+	class Meta(object):
+		db_table = 'fans_user_has_fans'
