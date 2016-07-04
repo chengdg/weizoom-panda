403466d9383ebb46af44ea4d87786818bc423fcc
lihanyi <lihanyi@weizoom.com>
2016-07-04 14:07:05 +0800

    add status

--- fans/models.py
+++ fans/models.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+DELIVERED = 0
+READED = 1
+SHARED = 2
+ORDERED = 3
+STATUS = (
+	(DELIVERED, u'已妥投，未阅读'),
+	(READED, u'已阅读，未分享'),
+	(SHARED, u'已阅读，已分享'),
+	(ORDERED, u'已下单')
+)
+STATUS2NAME = dict(STATUS)
 
+#===============================================================================
+# Fans ： 粉丝投放-粉丝库
+#===============================================================================
 class Fans(models.Model):
 	weibo_id = models.CharField(max_length=24, default='') #粉丝id
 	name = models.CharField(max_length=2048, default='') #粉丝名称
@@ -15,10 +28,14 @@ class Fans(models.Model):
 	class Meta(object):
 		db_table = 'fans_fans'
 
+
+#===============================================================================
+# UserHasFans ： 粉丝投放-已投放粉丝信息
+#===============================================================================
 class UserHasFans(models.Model):
 	user_id = models.IntegerField(default=0) #用户id
 	fans_id = models.IntegerField(default=0) #粉丝id
-	status = models.IntegerField(default=0) #投放状态
+	status = models.IntegerField(default=DELIVERED) #投放状态
 	related_order_id = models.CharField(max_length=1024, default='') #关联订单号
 	pushed_date = models.DateTimeField() #投放日期
 
