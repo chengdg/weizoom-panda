6cd213bdef3806b1dd41503dbca5c493c812dc11
lihanyi <lihanyi@weizoom.com>
2016-07-06 10:09:49 +0800

    m

--- account/management/commands/regular_create_user_has_fans.py
+++ account/management/commands/regular_create_user_has_fans.py
@@ -39,7 +39,7 @@ class Command(BaseCommand):
 			all_sellers = account_models.UserProfile.objects.filter(id__startswith=id_start_with,role=account_models.CUSTOMER)
 		#测试使用
 		all_sellers = account_models.UserProfile.objects.filter(id=3,role=account_models.CUSTOMER)
-		account_ids = [seller.account_id for seller in all_sellers]
+		account_ids = [seller.id for seller in all_sellers]
 		account_has_suppliers = account_models.AccountHasSupplier.objects.filter(account_id__in=account_ids)
 		today_supplier_ids = []
 		supplier_id2orders = {}
