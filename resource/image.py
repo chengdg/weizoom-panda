a714a778b64d0e9c9615e227c11fbc259e863dc7
huangjian <huangjian@narzh.com>
2016-07-27 16:10:33 +0800

    上传图片到又拍云

--- resource/image.py
+++ resource/image.py
@@ -16,6 +16,7 @@ from django.conf import settings
 from core import resource
 from core.jsonresponse import create_response
 from core.exceptionutil import unicode_full_stack
+from panda.settings import PANDA_HOST
 import models
 import base64
 import upyun
@@ -69,8 +70,12 @@ class Image(resource.Resource):
 		#保存图片信息到mysql中
 
 		image_path = '/static/upload/%s/%s' % (store_dir, file_name)
-		# value = save_and_zip_base64_img_file_for_mobileApp(request, file_path, image_path, file_name)
-		# print value,"============"
+
+		try:
+			value = upload_image_to_upyun(file_path,image_path)
+			image_path = value
+		except:
+			image_path = PANDA_HOST + '/static/upload/%s/%s' % (store_dir, file_name)
 
 		image = models.Image.objects.create(
 			user = request.user,
@@ -84,38 +89,20 @@ class Image(resource.Resource):
 		}
 		return response.get_response()
 
-########################################################################
-# save_and_zip_base64_img_file_for_mobileApp: 存储手机上传的图片压缩大于1M的图片
-########################################################################
-def save_and_zip_base64_img_file_for_mobileApp(request, file_path, image_path ,file_name):
-	date = time.strftime('%Y%m%d')
-	dir_path_suffix = 'panda/%d_%s' % (request.user.id, date)
-
-	try:
-		image_path = upload_image_to_upyun(file_path,image_path)
-		print image_path,"==========ttttt44========"
-		return image_path
-	except:
-		notify_msg = u"上传图片到又拍云时失败, cause:\n{}".format(unicode_full_stack())
-		print notify_msg
-		return '/static/upload/%s/%s' % (dir_path_suffix, file_name)
-
-
 image_path = "http://%s.b0.upaiyun.com%s"
 def upload_image_to_upyun(file_path, upyun_path):
 	if settings.MODE == 'test':
-		BUCKETNAME = 'testpanda'
+		BUCKETNAME = 'testweapp'
 	else:
-		BUCKETNAME = 'pandaimg'
+		BUCKETNAME = 'weappimg'
 	USERNAME = 'weizoom'
 	PASSWORD = 'weizoom_weapp'
 
 	if settings.MODE == 'develop':
-		return '/static%s' % upyun_path
+		return upyun_path
 		
 	up = upyun.UpYun(BUCKETNAME, USERNAME, PASSWORD, timeout=300,
 			endpoint=upyun.ED_AUTO)
-	#headers = {"x-gmkerl-rotate": "180"}
 	try:
 		with open(file_path, 'rb') as f:
 			try:
@@ -127,5 +114,5 @@ def upload_image_to_upyun(file_path, upyun_path):
 	except:
 		notify_message = u"upload_image_to_upyun error {}".format(unicode_full_stack())
 		print notify_message
-		return '/static%s' % upyun_path
+		return upyun_path
 	return None
