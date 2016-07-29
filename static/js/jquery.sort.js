be108560a8a3a547f967bbdb8978fdaf96625612
lihanyi <lihanyi@weizoom.com>
2016-07-29 11:06:29 +0800

    m

--- static/js/jquery.sort.js
+++ static/js/jquery.sort.js
@@ -1,9 +1,4 @@
-/**
- * jquery.sort.js
- * 商品发布-选择分类
- * author: 锐不可挡
- * date: 2016-07-07
-**/
+
 /*定义三级分类数据*/
 //一级分类
 var province = ["教育", "文艺", "青春", "生活", "人文社科", "经管", "科技", "电子书"];
@@ -18,42 +13,52 @@ var city = [
 	["科普读物", "建筑", "医学", "计算机/网络", "农业/林业", "自然科学", "工业技术"],
 	["新华出品", "文艺", "网络文学", "人文社科", "经管励志", "生活", "童书", "科技", "教育", "期刊杂志"]
 ];
-var expressP, expressC, expressD, expressArea, areaCont;
-var arrow = " <font>&gt;</font> ";
+var expressP, expressC, expressArea, areaCont;
+var arrow = '_';
 
 /*初始化一级目录*/
 function intProvince() {
 	areaCont = "";
+	expressC = '';
 	for (var i=0; i<province.length; i++) {
-		areaCont += '<li onClick="selectP(' + i + ');"><a href="javascript:void(0)">' + province[i] + '</a></li>';
+		areaCont += '<li onClick="selectA(' + i + ');"><a href="javascript:void(0)">' + province[i] + '</a></li>';
+		for (var j=0; j<city[i].length; j++) {
+			expressC += '<li class="selectB_'+i+' hide selectB_' + i + '_' + j + '" onClick="selectB(' + i + ',' + j + ');"><a href="javascript:void(0)">' + city[i][j] + '</a></li>';
+		}
+		$("#sort2").html(expressC)
 	}
 	$("#sort1").html(areaCont);
 }
 intProvince();
 
 /*选择一级目录*/
-function selectP(p) {
-	areaCont = "";
-	for (var j=0; j<city[p].length; j++) {
-		areaCont += '<li onClick="selectC(' + p + ',' + j + ');"><a href="javascript:void(0)">' + city[p][j] + '</a></li>';
+function selectA(p) {
+	$("#sort2 li").addClass("hide")
+	$('.selectB_'+p+'').removeClass("hide");//暴露出二级分类
+	// $("#sort1 li").eq(p).addClass("active").siblings("li").removeClass("active");
+	if($("#sort1 li").eq(p).hasClass("active")){
+		if(!$('.selectB_'+p+'').hasClass("active")){ //没有二级分类被选中时，不选择一级分类
+			$("#sort1 li").eq(p).removeClass("active");
+		}
+	}else{
+		$("#sort1 li").eq(p).addClass("active");
 	}
-	$("#sort2").html(areaCont).show();
-	$("#sort1 li").eq(p).addClass("active").siblings("li").removeClass("active");
-	expressP = province[p];
-	$("#selectedSort").html(expressP);
+	
+	// expressP = province[p];
+	// console.log('expressP',expressP);
+	// $("#selectedSort").html(expressP);
 }
 
 /*选择二级目录*/
-function selectC(p,c) {
-	areaCont = "";
-	expressC = "";
+function selectB(p,c) {
 	// $("#sort2 li").eq(c).addClass("active").siblings("li").removeClass("active");
-	if($("#sort2 li").eq(c).hasClass("active")){
-		$("#sort2 li").eq(c).removeClass("active");
+	// console.log($("#sort2 li").eq(c));
+	console.log($('.selectB_' + p + '_' + c + ''));
+	if($('.selectB_' + p + '_' + c + '').hasClass("active")   ){
+		$('.selectB_' + p + '_' + c + '').removeClass("active");
 	}else{
-		$("#sort2 li").eq(c).addClass("active");
+		$('.selectB_' + p + '_' + c + '').addClass("active");
 	}
-	expressC = expressP + arrow + city[p][c];
-	$("#selectedSort").html(expressC);
+	// console.log('city:',city[p][c]);
 }
 
