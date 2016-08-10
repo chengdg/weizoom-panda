06b95251b91bd97dc353f8baffb9fcc52a0caaa8
huangjian <huangjian@narzh.com>
2016-08-10 15:41:33 +0800

    验证

--- static/component/freight_service/freight/FreightPage.react.js
+++ static/component/freight_service/freight/FreightPage.react.js
@@ -36,12 +36,13 @@ var FreightPage = React.createClass({
 		var freeFreightMoney = this.state.free_freight_money;
 		var needFreightMoney = this.state.need_freight_money;
 		var reg = /^\+?[0-9][0-9]*$/;
+		var reg_2 = /^\+?[1-9][0-9]*$/;
 		if(freeFreightMoney.length>0 && !reg.test(freeFreightMoney)){
 			Reactman.PageAction.showHint('error', '请输入大于或等于0的整数!');
 			return;
 		}
-		if(!reg.test(needFreightMoney)){
-			Reactman.PageAction.showHint('error', '请输入大于或等于0的整数!');
+		if(!reg_2.test(needFreightMoney)){
+			Reactman.PageAction.showHint('error', '请输入大于0的整数!');
 			return;
 		}
 		Action.saveSalePhone(freeFreightMoney,needFreightMoney);
