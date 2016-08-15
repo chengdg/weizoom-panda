9bbd22fa2dc51d0d8598a54b75dc538ab5e8a5e5
lihanyi <lihanyi@weizoom.com>
2016-08-15 15:07:37 +0800

    去除不要的

--- fans/fans_exported.py
+++ fans/fans_exported.py
@@ -11,24 +11,24 @@ from core import resource
 from excel_response import ExcelResponse
 import models as fans_models
 
-class FansExported(resource.Resource):
-	app = 'fans'
-	resource = 'fans_exported'
+# class FansExported(resource.Resource):
+# 	app = 'fans'
+# 	resource = 'fans_exported'
 
-	def get(request):
-		user_has_fans = fans_models.UserHasFans.objects.filter(user_id=139)
-		fans_ids = [fans.fans_id for fans in user_has_fans]
-		fans = fans_models.Fans.objects.filter(id__in=fans_ids)
-		titles = [
-			u'昵称', u'购买指数',u'推荐传播指数'
-		]
-		table = []
-		table.append(titles)
-		for fan in fans:
-			table.append([
-				fan.name,
-				fan.purchasing_index,
-				fan.spread_index
-			])
+# 	def get(request):
+# 		user_has_fans = fans_models.UserHasFans.objects.filter(user_id=139)
+# 		fans_ids = [fans.fans_id for fans in user_has_fans]
+# 		fans = fans_models.Fans.objects.filter(id__in=fans_ids)
+# 		titles = [
+# 			u'昵称', u'购买指数',u'推荐传播指数'
+# 		]
+# 		table = []
+# 		table.append(titles)
+# 		for fan in fans:
+# 			table.append([
+# 				fan.name,
+# 				fan.purchasing_index,
+# 				fan.spread_index
+# 			])
 
-		return ExcelResponse(table,output_name=u'粉丝投放统计列表'.encode('utf8'),force_csv=False)
+# 		return ExcelResponse(table,output_name=u'粉丝投放统计列表'.encode('utf8'),force_csv=False)
