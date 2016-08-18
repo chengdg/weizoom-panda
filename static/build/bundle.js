b2d1a1e78d966c1f41aeb6ebe1623d634d8fc55f
huangjian <huangjian@narzh.com>
2016-08-18 17:05:27 +0800

    build

--- static/build/bundle.js
+++ static/build/bundle.js
@@ -269,16 +269,39 @@
 	})();
 	function runTimeout(fun) {
 	    if (cachedSetTimeout === setTimeout) {
+	        //normal enviroments in sane situations
 	        return setTimeout(fun, 0);
-	    } else {
-	        return cachedSetTimeout.call(null, fun, 0);
+	    }
+	    try {
+	        // when when somebody has screwed with setTimeout but no I.E. maddness
+	        return cachedSetTimeout(fun, 0);
+	    } catch (e) {
+	        try {
+	            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+	            return cachedSetTimeout.call(null, fun, 0);
+	        } catch (e) {
+	            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+	            return cachedSetTimeout.call(this, fun, 0);
+	        }
 	    }
 	}
 	function runClearTimeout(marker) {
 	    if (cachedClearTimeout === clearTimeout) {
-	        clearTimeout(marker);
-	    } else {
-	        cachedClearTimeout.call(null, marker);
+	        //normal enviroments in sane situations
+	        return clearTimeout(marker);
+	    }
+	    try {
+	        // when when somebody has screwed with setTimeout but no I.E. maddness
+	        return cachedClearTimeout(marker);
+	    } catch (e) {
+	        try {
+	            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+	            return cachedClearTimeout.call(null, marker);
+	        } catch (e) {
+	            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+	            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+	            return cachedClearTimeout.call(this, marker);
+	        }
 	    }
 	}
 	var queue = [];
@@ -22423,9 +22446,14 @@
 				this.$el = $node;
 			}
 			this.$el.animate({ top: '50px', opacity: 1 }, 300);
+
+			var delayTime = 3000; //ms
+			if (this.props.type === 'error') {
+				delayTime = 5000;
+			}
 			_.delay(_.bind(function () {
 				this.$el.animate({ opacity: 0 }, 1000).animate({ top: -50 });
-			}, this), 3000);
+			}, this), delayTime);
 
 			_.delay(_.bind(function () {
 				if (this.props.hint.length > 0) {
@@ -43901,6 +43929,25 @@
 						);
 					}
 				}
+			} else if (field === 'catalog_name') {
+				var name = data['second_level_name'];
+				var line = name.length > 0 ? '-' : '';
+				return React.createElement(
+					'div',
+					null,
+					React.createElement(
+						'span',
+						null,
+						data['first_level_name']
+					),
+					React.createElement('br', null),
+					React.createElement(
+						'span',
+						{ style: { paddingLeft: '10px' } },
+						line,
+						data['second_level_name']
+					)
+				);
 			} else {
 				return value;
 			}
@@ -43951,6 +43998,7 @@
 						Reactman.Table,
 						{ resource: productsResource, formatter: this.rowFormatter, pagination: true, ref: 'table' },
 						React.createElement(Reactman.TableColumn, { name: '商品信息', field: 'product_name', width: '400px' }),
+						React.createElement(Reactman.TableColumn, { name: '分类', field: 'catalog_name' }),
 						React.createElement(Reactman.TableColumn, { name: '结算价(元)', field: 'clear_price' }),
 						React.createElement(Reactman.TableColumn, { name: '售价(元)', field: 'product_price' }),
 						React.createElement(Reactman.TableColumn, { name: '销量', field: 'sales' }),
@@ -48766,6 +48814,10 @@
 			this.Action = Action(this.Dispatcher);
 			this.Store.addListener(this.onReloadData);
 
+			this.innerState = {
+				page: this.props.resource.data['page'] || 1
+			};
+
 			//加载数据
 			var autoLoad = true;
 			if (this.props.hasOwnProperty('autoLoad')) {
@@ -48785,7 +48837,7 @@
 			$table.delegate('a', 'click', function (event) {
 				var $link = $(event.target);
 				var href = $link.attr('href');
-				if (href.contains('__memorize')) {
+				if (href && href.contains('__memorize')) {
 					var top = $(window).scrollTop();
 					var url = _this.fullUrl + '&__r_top=' + top;
 					href += '&__r_rollback=' + encodeURIComponent(url);
@@ -48812,7 +48864,7 @@
 		onReloadData: function (event) {
 			var storeData = this.Store.getData();
 			var data = {};
-			data['pagination_info'] = storeData['pagination_info'];
+			data['paginationInfo'] = storeData.paginationInfo;
 			data['isAllRowSelected'] = storeData.isAllRowSelected;
 
 			var rows = storeData['rows'];
@@ -48838,7 +48890,7 @@
 		},
 
 		onChangePage: function (page) {
-			this.props.resource.data['page'] = page;
+			this.innerState.page = page;
 			this.__refresh(this.filterOptions);
 		},
 
@@ -48873,7 +48925,7 @@
 				var originalFilterStr = JSON.stringify(this.filterOptions);
 				var newFilterStr = JSON.stringify(filterOptions);
 				if (newFilterStr !== originalFilterStr) {
-					this.props.resource.data['page'] = 1;
+					this.innerState.page = 1;
 				}
 			}
 			this.__refresh(filterOptions);
@@ -48889,7 +48941,7 @@
 				this.rollbackInfo = System.getRollbackInfo();
 				System.clearRollbackInfo();
 
-				resource.data['page'] = this.rollbackInfo.page;
+				this.innerState.page = this.rollbackInfo.page;
 				if (this.rollbackInfo.filters) {
 					filterOptions = this.rollbackInfo.filters;
 				}
@@ -48899,6 +48951,7 @@
 			}
 
 			resource.data = _.clone(this.props.resource.data);
+			resource.data.page = this.innerState.page;
 			if (filterOptions) {
 				this.filterOptions = filterOptions;
 				_.extend(resource.data, filterOptions);
@@ -49045,7 +49098,7 @@
 			} else {
 				var tableInfo = this.createHeadAndRow();
 
-				var mPagination = this.createPagination(this.state['pagination_info']);
+				var mPagination = this.createPagination(this.state.paginationInfo);
 
 				var enableBorder = this.props.enableBorder === false ? false : true;
 				var enableHeader = this.props.enableHeader === false ? false : true;
@@ -49327,7 +49380,9 @@
 			},
 
 			handleReload: function (action) {
-				this.data = action.data;
+				this.data.rows = action.data['rows'];
+				this.data.paginationInfo = action.data['pagination_info'];
+				//this.data = action.data;
 				this.isAllRowSelected = false;
 				this.selectedRowIds = [];
 				this.__emitChange();
