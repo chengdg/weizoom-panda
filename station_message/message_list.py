e035e0bcc0cd5f32b6ae637de83fb5f7ffb769c9
charles <qiaozhanlei@weizoom.com>
2016-08-22 15:36:44 +0800

    站内信修改1.

--- station_message/message_list.py
+++ station_message/message_list.py
@@ -9,13 +9,14 @@ from eaglet.utils.resource_client import Resource
 from eaglet.core import watchdog
 
 from core import resource
+from core import paginator
 from core.jsonresponse import create_response
 
 from account import models as account_models
 from product import models as product_models
 from panda.settings import EAGLET_CLIENT_ZEUS_HOST, ZEUS_SERVICE_NAME
 
-import models as product_catalog_models
+import models as message_models
 import nav
 
 FIRST_NAV = 'message'
@@ -44,6 +45,12 @@ class ProductCatalog(resource.Resource):
 		"""
 
 		"""
-		print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..'
 
+		cur_page = request.GET.get('page', 1)
+		messages = message_models.Message.objects.filter(is_deleted=False).order_by('-created_at')
+
+		page_messages = paginator.paginate(messages, cur_page, 20)
+		print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..'
+		print page_messages
+		print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..'
 		return None
