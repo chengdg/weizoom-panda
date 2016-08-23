1ad8275fb51535890d1da871ac8394a8bb8cfcc7
lihanyi <lihanyi@weizoom.com>
2016-08-23 17:55:23 +0800

    接口

--- manager/api_account_create.py
+++ manager/api_account_create.py
@@ -15,7 +15,6 @@ from core import resource
 from core.jsonresponse import create_response
 from eaglet.utils.resource_client import Resource
 
-from core import paginator
 from util import db_util
 from panda.settings import ZEUS_HOST
 import nav
@@ -24,40 +23,40 @@ from account.models import *
 from panda.settings import ZEUS_SERVICE_NAME, EAGLET_CLIENT_ZEUS_HOST
 from product_catalog import models as product_catalog_models
 from manager.account_create import check_username_valid
-
+from eaglet.decorator import param_required
 
 class AccountCreateApi(resource.Resource):
-	app = 'panda'
-	resource = 'account_create'
+	app = 'manager'
+	resource = 'account_create_api'
 	
 	@param_required(['name', 'username', 'password', 'company_name', 'company_type', 'purchase_method', 'contacter', 'phone', 'valid_time_from', 'valid_time_to'])
-    def put(args):
+	def api_put(request):
 		"""
 		新建账号
 		"""
-        name = args['name']
-        username = args['username']
-        password = args['password']
-        company_name = args['company_name']
-        company_type = args['company_type']
-        purchase_method = args['purchase_method']
-        contacter = args['contacter']
-        phone = args['phone']
-        valid_time_from = args['valid_time_from']
-        valid_time_to = args['valid_time_to']
-        note = args.get('note', '')
-        points = args.get('points', 0)
-        order_money = args.get('order_money', 0)
-        rebate_proport = args.get('rebate_proport', 0)
-        default_rebate_proport = args.get('default_rebate_proport', 0)
+		args = request.POST
+		name = args['name']
+		username = args['username']
+		password = args['password']
+		company_name = args['company_name']
+		company_type = args['company_type']
+		purchase_method = args['purchase_method']
+		contacter = args['contacter']
+		phone = args['phone']
+		valid_time_from = args['valid_time_from']
+		valid_time_to = args['valid_time_to']
+		note = args.get('note', '')
+		points = args.get('points', 0)
+		order_money = args.get('order_money', 0)
+		rebate_proport = args.get('rebate_proport', 0)
+		default_rebate_proport = args.get('default_rebate_proport', 0)
 
-        if not check_username_valid(username):
-        	return {
-                'result': 'FAILED',
-                'msg': u"登录账号已存在，请重新输入"
-            }
-        try:
-        	user = User.objects.create_user(username,username+'@weizoom.com',password)
+		if not check_username_valid(username):
+			response = create_response(500)
+			response.errMsg = u'登录账号已存在，请重新输入'
+			return response.get_response()
+		try:
+			user = User.objects.create_user(username,username+'@weizoom.com',password)
 			user.first_name = name
 			user.save()
 			user_id = user.id
@@ -66,9 +65,7 @@ class AccountCreateApi(resource.Resource):
 				manager_id = 2,
 				role = 1,
 				name = name,
-				note = note
-			)
-			user_profile.update(
+				note = note,
 				company_name = company_name,
 				company_type = company_type,
 				purchase_method = purchase_method,
@@ -100,43 +97,36 @@ class AccountCreateApi(resource.Resource):
 							# store_name = account_zypt_info['store_name'].encode('utf8'),
 							supplier_id = int(supplier_datas['id'])
 						)
-						return {
-			                'result': 'SUCCESS',
-			                'msg': u"创建账号成功",
-			                'data':{
-			                	'supplier_id': int(supplier_datas['id']),
-								'account_id': user_profile[0].id
-			                }
-			            }
+						response = create_response(200)
+						response.data = {
+							'supplier_id': int(supplier_datas['id']),
+							'account_id': user_profile[0].id
+						}
+						return response.get_response()
 					pass
 				else:
 					User.objects.filter(id=user_id).delete()
 					UserProfile.objects.filter(user_id=user_id).delete()
-					return {
-		                'result': 'FAILED',
-		                'msg': u"ZEUS创建账号失败"
-		            }
-		    except Exception,e:
-				print(e)
+					response = create_response(500)
+					response.errMsg = u'ZEUS创建账号失败'
+					return response.get_response()
+			except Exception,e:
 				User.objects.filter(id=user_id).delete()
 				UserProfile.objects.filter(user_id=user_id).delete()
-				return {
-	                'result': 'FAILED',
-	                'msg': u"PANDA创建账号失败"
-	            }
+				response = create_response(500)
+				response.errMsg = u'PANDA创建账号失败'
+				return response.get_response()
 		except Exception,e:
-			print(e)
-			print('===========================')
-			return {
-                'result': 'FAILED',
-                'msg': u"PANDA创建账号失败"
-            }
+			response = create_response(500)
+			response.errMsg = u'PANDA创建账号失败:'
+			return response.get_response()
 
-    @param_required(['account_id', 'new_password'])
-    def post(args):
-    	"""
+	@param_required(['account_id', 'new_password'])
+	def api_post(request):
+		"""
 		修改密码
 		"""
+		args = request.POST
 		account_id = args['account_id']
 		password = args['new_password']
 		try:
@@ -145,25 +135,19 @@ class AccountCreateApi(resource.Resource):
 			user = User.objects.get(id=user_id)
 			user.set_password(password)
 			user.save()
-			return {
-                'result': 'SUCCESS',
-                'msg': u"修改密码成功"
-            }
+			response = create_response(200)
+			response.data = u"修改密码成功"
+			return response.get_response()
 		except Exception,e:
-			print(e)
-			print('===========================')
-			return {
-                'result': 'FAILED',
-                'msg': u"该账号不存在"
-            }
-
-
+			response = create_response(200)
+			response.data = u"该账号不存在:"
+			return response.get_response()
 
 class GetAllFirstCatalog(resource.Resource):
-	app = 'panda'
-	resource = 'get_all_first_catalog'
+	app = 'manager'
+	resource = 'get_all_first_catalog_api'
 
-	def get(args):
+	def api_get(request):
 		"""
 		获得一级经营类目
 		"""
@@ -174,6 +158,6 @@ class GetAllFirstCatalog(resource.Resource):
 				'text': catalog.name,
 				'value': catalog.id
 			})
-		return {
-            'datas': rows,
-        }
+		response = create_response(200)
+		response.data = rows
+		return response.get_response()
