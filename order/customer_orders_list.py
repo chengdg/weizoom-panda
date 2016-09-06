db1d79e70420b8b3956427a5061784ee95e16e8e
lihanyi <lihanyi@weizoom.com>
2016-09-06 14:51:08 +0800

    列表显示

--- order/customer_orders_list.py
+++ order/customer_orders_list.py
@@ -228,7 +228,7 @@ class CustomerOrdersList(resource.Resource):
 					response = create_response(500)
 					return response.get_response()
 
-			# price【商品单价】，purchase_price【结算价】，total_price【商品售价*数量】
+			# price【商品单价】，purchase_price【结算价】，total_price【商品售价*数量】 origin_price【售价】
 			for order in orders:
 				order_id = order['order_id']
 				product_infos = []
@@ -246,7 +246,7 @@ class CustomerOrdersList(resource.Resource):
 							'product_name': product_weapp_id2info[product_id][0]['product_name'],
 							'model_names': model_names,
 							'product_img': product_weapp_id2info[product_id][0]['product_img'],
-							'price': return_product_info['origin_price'],
+							'price': return_product_info['purchase_price'] if purchase_method == 1 else return_product_info['origin_price'],
 							'count': return_product_info['count']
 						})
 					else:
@@ -258,7 +258,7 @@ class CustomerOrdersList(resource.Resource):
 							'product_name': return_product_info['name'],
 							'model_names': model_names,
 							'product_img': return_product_info['thumbnails_url'],
-							'price': return_product_info['origin_price'],
+							'price': return_product_info['purchase_price'] if purchase_method == 1 else return_product_info['origin_price'],
 							'count': return_product_info['count']
 						})
 					if not is_for_list:
