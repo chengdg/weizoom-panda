e3e1f596545b9e664b567b93e1190af68cb8cba3
huangjian <huangjian@narzh.com>
2016-09-22 10:21:34 +0800

    代码调整

--- static/component/product_limit_zone/Store.js
+++ static/component/product_limit_zone/Store.js
@@ -15,32 +15,22 @@ var Constant = require('./Constant');
 
 var Store = StoreUtil.createStore(Dispatcher, {
 	actions: {
-
 		'handleUpdateTemplates': Constant.LIMIT_ZONE_TEMPLATE_UPDATE,
 	},
 
 	init: function() {
 		this.data = {
-			'models': [],
-			'propertyId2names': {},
-			'labelCatalogs': [],
-			'labelValues': []
+			
 		};
 	},
 
-	handleCatelogDataFilter: function(action){
-		this.data = action.data;
-		this.__emitChange();
-	},
-
-	getData: function() {
-		return this.data;
-	},
-
     handleUpdateTemplates: function(action){
-//        this.data.models = action.data.models;
 		this.__emitChange();
-    }
+    },
+
+    getData: function() {
+		return this.data;
+	}
 });
 
 module.exports = Store;
