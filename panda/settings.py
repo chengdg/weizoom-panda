0d54295104b5b3407f3b6039644ff6c7adce01d1
charles <qiaozhanlei@weizoom.com>
2016-10-10 09:15:16 +0800

    mns消息需求添加配置

--- panda/settings.py
+++ panda/settings.py
@@ -300,6 +300,14 @@ elif 'develop' == MODE:
     MNS_SECURITY_TOKEN = ''
     PRODUCT_TOPIC_NAME = 'test-topic'
     PRODUCT_MSG_NAME = 'test-queue'
+elif 'test' == MODE:
+    MNS_ACCESS_KEY_ID = 'LTAI0GgFxEnSn3yP'
+    MNS_ACCESS_KEY_SECRET = 'O3OWEDiBhqL4PDd5HCjxQyMM9QLo1G'
+    MNS_ENDPOINT = 'http://1615750970594173.mns.cn-beijing.aliyuncs.com/'
+    # MNS_ENDPOINT = 'http://1615750970594173.mns.cn-shanghai.aliyuncs.com/'
+    MNS_SECURITY_TOKEN = ''
+    PRODUCT_TOPIC_NAME = 'test-topic'
+    PRODUCT_MSG_NAME = 'test-queue'
 else:
     MNS_ACCESS_KEY_ID = 'eJ8LylRwQERRqOot'
     MNS_ACCESS_KEY_SECRET = 'xxPrfGcUlnsL7IPweJRqVekHTCu6Ad'
