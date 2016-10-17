f559493e3e8443fc06f3e23c5630cac1a9285065
likunlun <likunlun@weizoom.com>
2016-10-17 17:46:52 +0800

    修改bundle.js生成的路径

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 		]
 	},
 	output: {
-		path: path.resolve(__dirname, 'static'),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'bundle.js',
 		publicPath: '/static/'
 	},
