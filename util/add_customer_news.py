ed6d571ef70082bbb0ebe65c7728ceb8519a3f46
zhoukang <1120429511@qq.com>
2016-11-01 11:42:09 +0800

    方法

---
+++ util/add_customer_news.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from bdem import msgutil
+topic_name  = 'customer-news-add'
+
+def send_reject_product_message(product_name=None, reject_reason=None, customer_id=None, customer_name=None):
+	"""
+	商品驳回
+	"""
+	func_name = 'add_customer_news'
+	data = {
+		"name": "add_customer_news",
+		"data": {
+			'news': {
+				'author_username': 'youdongdong',
+				'author_name': '小微机器人',
+				'title': '审核通知',
+				'summary': '商品审核未通过（已通过）',
+				'description': '商品名：%s，已被操作为：入库审核驳回，驳回原因：%s' % (product_name, reject_reason),
+				'customer_id': 'wzc_%s' % (customer_id),
+				'customer_name': customer_name
+			}
+		}
+	}
+
+	msgutil.send_message(topic_name, func_name, data)
+
+
+def send_sync_product_message(product_name=None, platforms=[], customer_id=None, customer_name=None):
+	"""
+	商品同步
+	"""
+	func_name = 'add_customer_news'
+	platforms_string = '、'.join(platforms)
+	data = {
+		"name": "add_customer_news",
+		"data": {
+			'news': {
+				'author_username': 'youdongdong',
+				'author_name': '小微机器人',
+				'title': '审核通知',
+				'summary': '商品审核已通过',
+				'description': '商品名：%s，已被操作为：已入库，已同步，同步同平台分别为：%s' % (product_name, platforms_string),
+				'customer_id': 'wzc_%s' % (customer_id),
+				'customer_name': customer_name
+			}
+		}
+	}
+
+	msgutil.send_message(topic_name, func_name, data)
+
+
+def send_stop_sell_product_message(product_name=None, stop_reason=None, customer_id=None, customer_name=None):
+	"""
+	商品停售
+	"""
+	func_name = 'add_customer_news'
+	data = {
+		"name": "add_customer_news",
+		"data": {
+			'news': {
+				'author_username': 'youdongdong',
+				'author_name': '小微机器人',
+				'title': '停售通知',
+				'summary': '商品撤架停售',
+				'description': '商品名：%s，已被操作为：已入库，已停售，停售原因：%s' % (product_name, stop_reason),
+				'customer_id': 'wzc_%s' % (customer_id),
+				'customer_name': customer_name
+			}
+		}
+	}
+
+	msgutil.send_message(topic_name, func_name, data)
