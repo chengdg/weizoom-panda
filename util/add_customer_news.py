96d0d56e37e9249f9e894a5c8411e13cabd1e105
zhoukang <1120429511@qq.com>
2016-11-03 09:28:31 +0800

    test

--- util/add_customer_news.py
+++ util/add_customer_news.py
@@ -32,7 +32,6 @@ def send_sync_product_message(product_name=None, platforms=[], customer_id=None,
 	"""
 	func_name = 'add_customer_news'
 	platforms_string = u'、'.join(platforms)
-	print type(platforms_string,'9999999999999999999999999999')
 	data = {
 		"name": "add_customer_news",
 		"data": {
