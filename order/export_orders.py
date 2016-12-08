72bf7356ab7e1eb6af5e5c6f951e64cee6d37a7d
zhupenghui <zhupenghui@weizoom.com>
2016-12-08 09:50:56 +0800

    修改导出 其他 的BUG

--- order/export_orders.py
+++ order/export_orders.py
@@ -43,7 +43,8 @@ express_company_name2text = {
 	'rufengda': u'如风达',
 	'youshuwuliu': u'优速物流',
 	'annengwuliu': u'安能物流',
-	'yuanchengkuaiyun':'远成快运'
+	'yuanchengkuaiyun':'远成快运',
+	'qita':'其他'
 }
 
 class ExportOrders(resource.Resource):
