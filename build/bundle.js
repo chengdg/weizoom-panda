6cc1f95f0ec243e6dd396c540d7f51f1c3e3df47
zhupenghui <zhupenghui@weizoom.com>
2016-12-28 16:10:40 +0800

    去掉console

--- build/bundle.js
+++ build/bundle.js
@@ -39975,12 +39975,10 @@
 				// 		<div><GroupPointsDialog /></div>
 				// 	</div>
 				// )
-				console.log(this.props.points, 22222222);
-				var cur_points = this.props.points == ' ' ? this.props.points : 0;
 				return React.createElement(
 					'div',
 					{ className: 'account-create-purchase-method' },
-					React.createElement(Reactman.FormInput, { label: '\u96F6\u552E\u6263\u70B9:', type: 'text', name: 'points', validate: 'require-notempty', value: cur_points, onChange: this.props.onChange }),
+					React.createElement(Reactman.FormInput, { label: '\u96F6\u552E\u6263\u70B9:', type: 'text', name: 'points', validate: 'require-notempty', value: this.props.points, onChange: this.props.onChange }),
 					React.createElement(
 						'span',
 						{ className: 'money_note' },
